ea34fc5f7a33d0137bc25237fedf027e3ac5a23c
Agustin Kassis <webmaster@masize.com>
2024-09-06 19:09:02 -0300

    apply useNostrHooks

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,12 +2,14 @@
 
 import { AuthProvider } from "@/contexts/auth";
 import { DomainProvider } from "@/contexts/domain";
+import { useNostrHooks } from "nostr-hooks";
 
 interface AppProviderProps {
   children: React.ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps) => {
+  useNostrHooks();
   return (
     <AuthProvider>
       <DomainProvider>{children}</DomainProvider>
