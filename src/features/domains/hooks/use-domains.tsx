ddc799b20df26162e5eda19a0ade7199db60657b
Agustin Kassis <webmaster@masize.com>
2024-09-09 18:21:20 -0300

    remove addDomainViaHandle from useDomains

--- src/features/domains/hooks/use-domains.tsx
+++ src/features/domains/hooks/use-domains.tsx
@@ -6,7 +6,5 @@ export interface UseDomainsReturn extends DomainContextType {}
 export default function useDomains(): UseDomainsReturn {
   const domainContext = useContext(DomainContext);
 
-  const addDomainViaHandle = async (handle: string) => {};
-
   return domainContext;
 }
